6cb98d3fa7e9fe8525320a8cd0205005bc8ebbf6
Stéphane ROSE <srose@turbulent.ca>
2019-05-23 12:46:16 -0300

    Added: init script for db + beta script update with new tags name

--- db/migrations/beta.js
+++ db/migrations/beta.js
@@ -14,3 +14,18 @@ db.gamers.update({ region: 'la2' }, { $set: { region: 'las' } }, { multi: true }
 db.gamers.update({ region: 'oc1' }, { $set: { region: 'oce' } }, { multi: true });
 db.gamers.update({ region: 'ru' }, { $set: { region: 'ru' } }, { multi: true });
 db.gamers.update({ region: 'tr1' }, { $set: { region: 'tr' } }, { multi: true });
+
+// Update tags
+
+db.tags.update({ name: "Shot Caller" }, { $set: { name: "shotCaller" } });
+db.tags.update({ name: "Chill" }, { $set: { name: "chill" } });
+db.tags.update({ name: "Skilled" }, { $set: { name: "skilled" } });
+db.tags.update({ name: "Team Player" }, { $set: { name: "teamPlayer" } });
+db.tags.update({ name: "Abusive" }, { $set: { name: "abusive" } });
+db.tags.update({ name: "Quitter" }, { $set: { name: "quitter" } });
+db.tags.update({ name: "Unhelpful" }, { $set: { name: "unhelpful" } });
+db.tags.update({ name: "Unskilled" }, { $set: { name: "unskilled" } });
+db.tags.update({ name: "Cheater" }, { $set: { name: "cheater" } });
+db.tags.update({ name: "Griefer" }, { $set: { name: "griefer" } });
+db.tags.update({ name: "Troll" }, { $set: { name: "troll" } });
+db.tags.update({ name: "Scammer" }, { $set: { name: "scammer" } });
