56a1f5401c210966399dd0df3e58726b6823a63e
Stéphane ROSE <srose@turbulent.ca>
2019-06-20 20:56:24 -0300

    Added parsing for cors request instead of accepting everybody

--- app.js
+++ app.js
@@ -64,11 +64,18 @@ if (app.get('env') === 'production' || app.get('env') === 'staging') {
 // }
 
 var allowCrossDomain = function(req, res, next) {
-  // if (app.get('env') !== 'production') {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-  // }
+  let allowedOrigins = ['https://dev.gamerscout.com', 'https://gamerscout.com'];
+  if (app.get('env') === 'development') {
+    allowedOrigins.push('http://localhost:8081');
+    allowedOrigins.push('http://localhost:8080');
+  }
+  const origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 }
 // view engine setup
