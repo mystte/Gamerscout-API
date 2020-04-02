21d20e263fe6514a623ae945e6495ee0d19a1ff4
Stéphane ROSE <srose@turbulent.ca>
2020-04-01 23:47:04 -0400

    Put redirection on / for api

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var config = require('../config');
+var config = require("../config");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  // if (req.env === 'development') {
-  //   res.redirect(config.website_devel_url);
-  // } else {
-  //   res.redirect(config.website_prod_url);
-  // }
-  res.status(200).json({'status': 'OK'});
+router.get("/", function(req, res, next) {
+  if (req.env === "development") {
+    res.redirect("https://" + config.client.staging.url);
+  } else {
+    res.redirect("https://" + config.client.prod.url);
+  }
+  res.status(200).json({ status: "OK" });
 });
 
 module.exports = router;
