e3c05828935e261a6d18ed1885aed986e522fb6b
Stéphane ROSE <srose@turbulent.ca>
2021-02-06 10:43:11 -0500

    Added ssl

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,10 @@ var config = require("../config");
 /* GET home page. */
 router.get("/", function(req, res, next) {
   if (req.env === "development") {
-    res.redirect("https://" + config.client.staging.url);
+    return res.redirect("https://" + config.client.staging.url);
   } else {
-    res.redirect("https://" + config.client.prod.url);
+    return res.redirect("https://" + config.client.prod.url);
   }
-  res.status(200).json({ status: "OK" });
 });
 
 module.exports = router;
